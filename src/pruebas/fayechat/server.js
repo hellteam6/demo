564f3ee092b0e21d3f29a05753ebd2e7ace1938c
Manu <manu@ragtime.(none)>
2012-04-26 12:16:25 +0200

    Ejemplo de chat utilizando faye / nodejs y bayeux

---
+++ src/pruebas/fayechat/server.js
@@ -0,0 +1,29 @@
+var http = require('http'),
+	fs = require('fs'),
+	faye = require('faye');
+
+var index;
+fs.readFile('./index.html', function(err, data) {
+	if(err) {
+		throw err;
+	} else {
+		index = data;
+	}
+});
+
+var bayeux = new faye.NodeAdapter({ 
+	mount: '/fayechat', 
+	timeout: 45
+});
+
+function requestHandler(request, response) {
+	//console.log("Request received.");
+	response.writeHead(200, {'Content-Type': 'text/html'} );
+	response.write(index);
+	response.end();
+}
+
+var server = http.createServer(requestHandler);
+
+bayeux.attach(server);
+server.listen(8000);

26dd9cc352b2cda1a39e4fab28af4a13d1eaed04
Manu <manu@ragtime.(none)>
2012-05-01 23:39:26 +0200

    cambios explicados en el commit anterior

--- src/pofs/server.js
+++ src/pofs/server.js
@@ -4,28 +4,44 @@ var http = require('http'),
 	fs = require('fs'),
 	faye = require('faye');
 
+// url rewrites
+function rewrite(uri) {
+	var result = process.cwd();
+	var res = uri.match(/\/dojo\/(.*?)$/);
+	if(res != null) {
+		result = path.join(result,"../libs/dojo/"+res[1]);
+	} else {
+		result = path.join(process.cwd(),process.argv[2])
+		result = path.join(result,uri);
+
+		if(result.match(/\/$/)) {
+			result = path.join(result,"index.html");
+		}
+	}
+	return result;
+}
+
 // http server
 var server = http.createServer( function(request, response) {
 
 	var uri = url.parse(request.url).pathname;
-	console.log('GET '+uri);
-    var filename = path.join(process.cwd()+'/'+process.argv[2]+'/', uri);
-	console.log('file: '+filename);	  
+	var filename = rewrite(uri);
+
+	console.log("[*] HTTP GET \""+uri+"\"");
+	console.log("[*] serving file: \""+filename+"\"");	  
 
 	path.exists(filename, function(exists) {
     	if(!exists) {
+			console.log("[!] HTTP 404: \""+filename+"\"");
 			response.writeHead(404, {"Content-Type": "text/plain"});
 			response.write("404 Not Found\n");
 			response.end();
 			return;
 		}
 
-		if (fs.statSync(filename).isDirectory()) { 
-			filename += '/index.html';
-		}
-	
 		fs.readFile(filename, function(error, data) {
 			if(error) {
+				console.log("[!] HTTP 500: \""+error+"\"");
 				response.writeHead(500);
 				response.end();
 			} else {
@@ -42,6 +58,28 @@ var bayeux = new faye.NodeAdapter({
 	mount: '/bayeuxhandler', 
 	timeout: 45
 });
+
+// Monitoring
+bayeux.bind('handshake', function(clientId) {
+	console.log("[*] bayeux: client \"%s\" connected.", clientId);
+});
+bayeux.bind('subscribe', function(clientId, channel) {
+	console.log("[*] bayeux: client \"%s\" subscribed to channel: \"%s\".", clientId, channel);
+});
+bayeux.bind('unsubscribe', function(clientId, channel) {
+	console.log("[*] bayeux: client \"%s\" unsubscribed from channel: \"%s\".", clientId, channel);
+});
+bayeux.bind('publish', function(clientId, channel, data) {
+	console.log("[*] bayeux: client \"%s\" sent event to channel: \"%s\":", clientId, channel);
+	console.log("          %j", data);
+});
+bayeux.bind('disconnect', function(clientId) {
+	console.log("[*] bayeux: client \"%s\" disconnected.", clientId);
+});
+
+
 bayeux.attach(server);
+console.log("[*] Faye attached to server.");
 
 server.listen(8000);
+console.log("[*] Server listening on port 8000.");

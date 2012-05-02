8a6652513bfd5cf1816746c0625c78b6ff34ff58
aoi <aoi@ks209234.kimsufi.com>
2012-05-02 21:02:32 +0200

    Agregado soporte para cache (respuesta HTTP 304) al server.js

--- src/pofs/server.js
+++ src/pofs/server.js
@@ -7,6 +7,7 @@ var http = require('http'),
 // url rewrites
 function rewrite(uri) {
 	var result = process.cwd();
+
 	var res = uri.match(/\/dojo\/(.*?)$/);
 	if(res != null) {
 		result = path.join(result,"../libs/dojo/"+res[1]);
@@ -51,26 +52,41 @@ var server = http.createServer( function(request, response) {
 	console.log("[*] HTTP GET \"%s\"", uri);
 	console.log("[*] serving file: \"%s\" with content type \"%s\".", filename, contentType);	  
 
-	path.exists(filename, function(exists) {
-    	if(!exists) {
-			console.log("[!] HTTP 404: \""+filename+"\"");
-			response.writeHead(404, {"Content-Type": "text/plain"});
-			response.write("404 Not Found\n");
-			response.end();
-			return;
-		}
-
-		fs.readFile(filename, function(error, data) {
-			if(error) {
-				console.log("[!] HTTP 500: \""+error+"\"");
-				response.writeHead(500);
-				response.end();
+	fs.readFile(filename, function(error, data) {
+		if(error) {
+			if(error.errno === 34) {
+				response.statusCode = 404;
+				console.log("[!] HTTP ERROR 404");
 			} else {
-				response.writeHead(200, {'Content-Type': contentType} );
-				response.write(data);
-				response.end();
+				response.statusCode = 500;
+				console.log("[!] HTTP ERROR 500");
 			}
-		});
+			response.end();
+		} else {
+			fs.stat(filename, function(error, stat) {
+				if(error) {
+					response.statusCode = 500;
+					console.log("[!] HTTP ERROR 500");
+					response.end();
+				} else {
+					etag = stat.size + '-' + Date.parse(stat.mtime);
+					response.setHeader('Last-Modified', stat.mtime);
+
+					if(request.headers['if-none-match'] === etag) {
+						response.statusCode = 304;
+						console.log("[*] HTTP Not modified 304");
+						response.end();
+					} else {
+						response.setHeader('Content-Length', data.length);
+						response.setHeader('Content-Type', contentType);
+						response.setHeader('ETag', etag);
+						response.statusCode = 200;
+						response.write(data);
+						response.end();
+					}
+				}
+			});
+		}
 	});
 });
 
@@ -79,8 +95,7 @@ var bayeux = new faye.NodeAdapter({
 	mount: '/bayeuxhandler', 
 	timeout: 45
 });
-
-// Monitoring
+// bayeux monitoring
 bayeux.bind('handshake', function(clientId) {
 	console.log("[*] ######## bayeux: client \"%s\" connected.", clientId);
 });
@@ -97,10 +112,10 @@ bayeux.bind('publish', function(clientId, channel, data) {
 bayeux.bind('disconnect', function(clientId) {
 	console.log("[*] ######## bayeux: client \"%s\" disconnected.", clientId);
 });
-
-
 bayeux.attach(server);
 console.log("[*] Faye attached to server.");
+
+
 
 server.listen(8000);
 console.log("[*] Server listening on port 8000.");
